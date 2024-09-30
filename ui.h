e56a00efa90fa9a8737a363cccbf175ed29268a1
Muhammed Rajab <muhammedrajab101@gmail.com>
2024-09-30 22:21:24 +0530

    added a better way to keep track of y's

--- ui.h
+++ ui.h
@@ -18,49 +18,62 @@ void RenderGUI(GA &ga, float screenWidth, float screenHeight, float renderXPos, 
     // * WITHIN BOX
     // * ----------------------------------------------------------
     // Rectangle groupBoxBounds{-1, 10, 200, GetMonitorHeight(0) - 20};
+    float y = 20.0f;
     Rectangle groupBoxBounds{-1, 10, 200, 280};
 
     // ! BORDER BOX
     GuiDrawRectangle(groupBoxBounds, 1, WHITE, Color{0, 0, 0, 0});
 
     // ! SETTINGS LABEL
-    GuiLabel({10, 20, 100, 20}, "PROPERTIES");
+    GuiLabel({10, y, 100, 20}, "SETTINGS");
+    y += 20;
 
     // ! CHECK BOX
-    (GuiCheckBox({10, 40, 20, 20}, "border", &ga.border));
+    (GuiCheckBox({10, y, 20, 20}, "border", &ga.border));
+    y += 25;
 
     // ! HUE SLIDER
     float hue = ga.BACKGROUND.h;
-    GuiLabel({10, 65, 100, 20}, "HUE");
-    if (GuiSlider({10, 85, 150, 20}, "", TextFormat("%d", ga.BACKGROUND.h), &hue, 0, 360))
+    GuiLabel({10, y, 100, 20}, "HUE");
+    y += 25;
+    if (GuiSlider({10, y, 150, 20}, "", TextFormat("%d", ga.BACKGROUND.h), &hue, 0, 360))
     {
         ga.BACKGROUND.h = (int)hue;
     }
+    y += 25;
 
     // ! ELITE RATIO SLIDER
     float elite_ratio = ga.ELITE_RATIO;
-    GuiLabel({10, 110, 100, 20}, "ELITE RATIO");
-    if (GuiSlider({10, 135, 150, 20}, "", TextFormat("%.3f", elite_ratio), &elite_ratio, 0.0005, 1.0))
+    GuiLabel({10, y, 100, 20}, "ELITE RATIO");
+    y += 25;
+    if (GuiSlider({10, y, 150, 20}, "", TextFormat("%.3f", elite_ratio), &elite_ratio, 0.0005, 1.0))
     {
         ga.ELITE_RATIO = elite_ratio;
     }
+    y += 25;
 
     // ! SPEED MULTIPLIER SLIDER
-    GuiLabel({10, 160, 100, 20}, "SPEED MULTIPLIER");
-    if (GuiSlider({10, 185, 150, 20}, "", TextFormat("%.3f", speedMultiplier), &speedMultiplier, 0.001, 60.0))
+    GuiLabel({10, y, 150, 20}, "SPEED MULTIPLIER");
+    y += 25;
+    if (GuiSlider({10, y, 150, 20}, "", TextFormat("%.3f", speedMultiplier), &speedMultiplier, 0.001, 60.0))
     {
         ga.ELITE_RATIO = elite_ratio;
     }
+    y += 25;
 
     // ! STATS
     Stats s = ga.Stat();
 
     // ! TOP FITNESS
-    GuiLabel({10, 210, 150, 20}, TextFormat("TOP FITNESS: %d", s.TOP_FITNESS_SCORE));
+
+    GuiLabel({10, y, 150, 20}, TextFormat("TOP FITNESS: %d", s.TOP_FITNESS_SCORE));
+    y += 25;
     // ! MEDIAN FITNESS
-    GuiLabel({10, 235, 150, 20}, TextFormat("MEDIAN FITNESS: %d", s.MEDIAN_FITNESS_SCORE));
+    GuiLabel({10, y, 150, 20}, TextFormat("MEDIAN FITNESS: %d", s.MEDIAN_FITNESS_SCORE));
+    y += 25;
     // ! WORST FITNESS
-    GuiLabel({10, 260, 150, 20}, TextFormat("WORST FITNESS: %d", s.WORST_FITNESS_SCORE));
+    GuiLabel({10, y, 150, 20}, TextFormat("WORST FITNESS: %d", s.WORST_FITNESS_SCORE));
+    y += 25;
 
     // * ----------------------------------------------------------
     // * OUT OF BOX
